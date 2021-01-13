f9a67265bfad7f3bdc384caffd97b32f750a7e79
Anna Agozzino <anna.agozzino91@gmail.com>
2021-01-13 16:02:37 +0100

    Add image and input value

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -3,11 +3,15 @@ const app = Vue.createApp({
         return{
             name: 'Anna',
             age: 29,
+            imgLink: 'https://picsum.photos/200/300'
         }
     },
     methods: {
         addFive(){
             return this.age + 5;
+        },
+        randomNumber() {
+            return Math.random();
         }
     }
 });
