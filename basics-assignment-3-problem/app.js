daac183fd49769a9b4c66810e7de14930022a7bf
Anna Agozzino <anna.agozzino91@gmail.com>
2021-01-20 14:41:27 +0100

    Assignment 3 - Add watcher with setTimeout

--- basics-assignment-3-problem/app.js
+++ basics-assignment-3-problem/app.js
@@ -5,19 +5,27 @@ const app = Vue.createApp({
         }    
     },
     computed: {
-        commentResult(){
+        commentResult() {
             // console.log(this.result);
             if (this.result < 37) {
-                return 'Not there yet!'
+                return 'Not there yet!';
             } 
             else if (this.result > 37) {
-                return 'Too much!'
+                return 'Too much!';
             }
-            return this.result
+            return this.result;
+        }
+    },
+    watch: {
+        commentResult() {
+            const that = this;
+            setTimeout(function() {
+                that.result = 0;
+            },5000);
         }
     },
     methods: {
-        add(num){
+        add(num) {
             this.result += num;
         }
     }
